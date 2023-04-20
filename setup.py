7fc09bce0183fbfa909e3dfcab1689fccece5463
mr-tz <moritz.raabe@mandiant.com>
2023-04-20 14:54:18 +0200

    update dependencies and fix

--- setup.py
+++ setup.py
@@ -8,8 +8,8 @@ import setuptools
 
 requirements = [
     "tabulate==0.9.0",
-    "vivisect==1.0.8",
-    "viv-utils[flirt]==0.7.7",
+    "vivisect==1.1.1",
+    "viv-utils[flirt]==0.7.9",
     "pydantic==1.10.7",
     "tqdm==4.65.0",
     "networkx==2.5.1",
