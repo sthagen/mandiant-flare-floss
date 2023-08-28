9c2a3cf4d6bb336f386c548e5fc6f2c5fc24722a
mr-tz <moritz.raabe@mandiant.com>
2023-08-28 13:20:51 +0200

    improve language warning

--- floss/main.py
+++ floss/main.py
@@ -547,9 +547,16 @@ def main(argv=None) -> int:
 
     # set language configurations
     if (lang_id == Language.GO and args.language == "") or args.language == Language.GO.value:
+        logger.warning(
+            "FLOSS handles Go static strings, but string deobfuscation may be inaccurate and take a long time"
+        )
         results.metadata.language = Language.GO.value
 
     elif (lang_id == Language.RUST and args.language == "") or args.language == Language.RUST.value:
+        if analysis.enable_tight_strings or analysis.enable_stack_strings or analysis.enable_decoded_strings:
+            logger.warning(
+                "FLOSS handles Rust static strings, but string deobfuscation may be inaccurate and take a long time"
+            )
         results.metadata.language = Language.RUST.value
 
     elif (lang_id == Language.DOTNET and args.language == "") or args.language == Language.DOTNET.value:
