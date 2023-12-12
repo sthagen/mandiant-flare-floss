3782dc95066a61c259eb08e86275a1b623592dec
Moritz <mr-tz@users.noreply.github.com>
2023-12-12 11:27:12 +0100

    Update version.py

--- floss/version.py
+++ floss/version.py
@@ -1,3 +1,3 @@
 # Copyright (C) 2017 Mandiant, Inc. All Rights Reserved.
 # caution: this file gets overwritten when building using PyInstaller, don't add required data in here without handling
-__version__ = "3.0.0"
+__version__ = "3.0.1"
