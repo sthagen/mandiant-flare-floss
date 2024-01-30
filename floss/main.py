b2ca8adfc5edf278861dd6bff67d73da39683b46
Arnav Kharbanda <94680887+Arker123@users.noreply.github.com>
2024-01-30 18:38:11 +0530

    Fix: Handle default prompt when stdout is redirected (#938)
    * when stdout is redirected, use default prompt values

--- floss/main.py
+++ floss/main.py
@@ -598,7 +598,11 @@ def main(argv=None) -> int:
 
     if results.metadata.language not in ("", "unknown"):
         if args.enabled_types == [] and args.disabled_types == []:
-            prompt = input("Do you want to enable string deobfuscation? (this could take a long time) [y/N] ")
+            # when stdout is redirected, such as in 'floss foo.exe | less' use default prompt values
+            if sys.stdout.isatty():
+                prompt = input("Do you want to enable string deobfuscation? (this could take a long time) [y/N] ")
+            else:
+                prompt = "n"
 
             if prompt.lower() == "y":
                 logger.info("enabled string deobfuscation")
