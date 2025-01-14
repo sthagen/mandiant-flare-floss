ab07022ff320b2595a057395d60c158fc8f15faa
Ana Maria Martinez Gomez <anamg@google.com>
2025-01-14 17:52:22 +0100

    [copyright + license] Fix headers
    Replace the header from source code files using the following script:
    ```Python
    for dir_path, dir_names, file_names in os.walk("flare-floss"):
        for file_name in file_names:
            try:
                file_path = f"{dir_path}/{file_name}"
                f = open(file_path, "rb+")
                content = f.read()
                m = re.search(OLD_HEADER, content)
                if not m:
                    continue
                print(f"{file_path}: {m.group('year')}")
                content = content.replace(m.group(0), NEW_HEADER % m.group("year"))
                f.seek(0)
                f.write(content)
            except:
                continue
    ```
    
    Some files had the copyright headers inside a `"""` comment and needed
    manual changes before applying the script.
    
    The old header had the confusing sentence `All rights reserved`, which
    does not make sense for an open source license. Replace the header by
    the default Google header that corrects this issue and keep floss
    consistent with other Google projects.

--- scripts/extract_rust_hashes.py
+++ scripts/extract_rust_hashes.py
@@ -1,5 +1,18 @@
 #!/usr/bin/env python3
-# Copyright (C) 2023 Mandiant, Inc. All Rights Reserved.
+# Copyright 2023 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 
 """
 File name: extract_rust_hashes.py
@@ -9,14 +22,6 @@ Description: Generates a database of Rust hashes from the Rust repository. Repo:
 Usage:
 
   $ python3 extract_rust_hashes.py
-
-Copyright (C) 2023 Mandiant, Inc. All Rights Reserved.
-Licensed under the Apache License, Version 2.0 (the "License");
- you may not use this file except in compliance with the License.
-You may obtain a copy of the License at: [package root]/LICENSE.txt
-Unless required by applicable law or agreed to in writing, software distributed under the License
- is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and limitations under the License.
 """
 
 import subprocess
@@ -55,7 +60,20 @@ print("Writing it to rust_version_database.py...")
 
 # write the hashes to a file
 header = """
-# Copyright (C) 2023 Mandiant, Inc. All Rights Reserved.
+# Copyright 2023 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 
 #############################################################################################
 # File: rust_version_database.py
